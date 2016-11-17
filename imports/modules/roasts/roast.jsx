56b3c495765d794d2a6b012c18d10e07e895e85d
Sven Röttering <svenroettering@googlemail.com>
2016-11-17 18:51:41 +0100

    finished general roast layout. logic needs to be implemented.

--- imports/modules/roasts/roast.jsx
+++ imports/modules/roasts/roast.jsx
@@ -15,14 +15,17 @@ class RoastC extends Component {
     this.renderScore = this.renderScore.bind(this);
     this.renderCommentSection = this.renderCommentSection.bind(this);
     this.renderComment = this.renderComment.bind(this);
+    this.renderCommentControls = this.renderCommentControls.bind(this);
+    this.renderContent = this.renderContent.bind(this);
+    this.renderReplies = this.renderReplies.bind(this);
   }
 
   renderHeadline() {
     return (
       <div className="roast-section">
-          <div>
-              <h2>Hi, I'm {this.props.roast.userName}, #roastme !</h2>
-          </div>
+        <div>
+          <h2>Hi, I'm { this.props.roast.userName }, #roastme!</h2>
+        </div>
       </div>
     );
   }
@@ -30,9 +33,9 @@ class RoastC extends Component {
   renderRoastImage() {
     return (
       <div className="roast-section">
-          <div>
-              <img className="roast-image" src={this.props.roast.imageUrl} alt="" />
-          </div>
+        <div>
+          <img className="roast-image" src={ this.props.roast.imageUrl } alt="" />
+        </div>
       </div>
     );
   }
@@ -40,11 +43,11 @@ class RoastC extends Component {
   renderSocials() {
     return (
       <div className="roast-social-media">
-          <ul>
-              <li><a href="#" className="button mdi mdi-facebook"><span>Facebook</span></a></li>
-              <li><a href="#" className="button mdi mdi-twitter"><span>Twitter</span></a></li>
-              <li><a href="#" className="button mdi mdi-google-plus"><span>Google +</span></a></li>
-          </ul>
+        <ul>
+          <li><a href="#" className="button mdi mdi-facebook"><span>Facebook</span></a></li>
+          <li><a href="#" className="button mdi mdi-twitter"><span>Twitter</span></a></li>
+          <li><a href="#" className="button mdi mdi-google-plus"><span>Google +</span></a></li>
+        </ul>
       </div>
     );
   }
@@ -52,8 +55,8 @@ class RoastC extends Component {
   renderScore() {
     return (
       <div className="roast-score">
-          <p className="big">{this.props.totalComments}<span className="mdi mdi-fire"></span></p>
-          <p className="big">{this.props.roast.totalUpvotes}<span className="mdi mdi-trophy-award"></span></p>
+        <p className="big">{ this.props.totalComments }<span className="mdi mdi-fire"></span></p>
+        <p className="big">{ this.props.roast.totalUpvotes }<span className="mdi mdi-trophy-award"></span></p>
       </div>
     );
   }
@@ -62,36 +65,71 @@ class RoastC extends Component {
     return (
       <div className="roast-section">
         { comments.map((comment, index) => {
-          return (
-            <div key={index}>
-              { this.renderComment(comment) }
-            </div>
-          );
-        })}
+          return this.renderComment(comment);
+        }) }
       </div>
     );
   }
 
   renderComment(comment) {
     return (
+      <div key={ comment._id }>
+        { this.renderContent({
+          userImage: comment.userImage,
+          userName: comment.userName,
+          votes: comment.votes.length,
+          content: comment.content,
+          commentId: comment._id,
+        }) }
+        { this.renderReplies(comment) };
+      </div>
+    );
+  }
+
+  renderReplies(comment) {
+    return (
+      <div className="roast-comment-answers">
+        { comment.replies.map((reply, index) => {
+          return (
+            <div className="roast-answer" key={ reply._id }>
+              { this.renderContent({
+                userImage: reply.userImage,
+                userName: reply.userName,
+                votes: reply.votes.length,
+                content: reply.content,
+                commentId: reply._id,
+              }) }
+            </div>
+          );
+        }) }
+      </div>
+    );
+  }
+
+  renderContent({userImage, userName, votes, content, commentId}) {
+    return (
       <div className="roast-comment">
-          <div className="roast-comment-profile">
-              <img src={comment.userImage} alt="" />
-              <h3>{comment.userName}</h3>
-              <p className="big">{comment.votes.length}<span className="mdi mdi-trophy-award"></span></p>
-          </div>
-          <div className="roast-commtent-text">
-              <p>
-                  {comment.content}
-              </p>
-          </div>
-          <div className="roast-comment-reply">
-              <ul>
-                  <li><a href="#" className="button mdi mdi-reply"><span>Reply</span></a></li>
-                  <li><a href="#" className="button mdi mdi-arrow-up-bold-circle"><span>Upvote</span></a></li>
-                  <li><a href="#" className="button mdi mdi-arrow-down-bold-circle"><span>Downvote</span></a></li>
-              </ul>
-          </div>
+        <div className="roast-comment-profile">
+          <img src={ userImage } alt="" />
+          <h3>{ userName }</h3>
+          <p className="big">{ votes } Points<span className="mdi mdi-trophy-award"></span></p>
+        </div>
+        <div className="roast-comment-text">
+          <p>{ content }</p>
+        </div>
+        { this.renderCommentControls(commentId) }
+      </div>
+    );
+  }
+
+  renderCommentControls(commentId) {
+    return (
+      <div className="roast-comment-reply">
+        <ul>
+          <li><a href="#" className="button mdi mdi-reply"><span>Reply</span></a></li>
+          <li><a href="#" className="button mdi mdi-arrow-up-bold-circle"><span>Upvote</span></a></li>
+          <li><a href="#" className="button mdi mdi-arrow-down-bold-circle"><span>Downvote</span></a></li>
+        </ul>
       </div>
     );
   }
@@ -100,15 +138,15 @@ class RoastC extends Component {
     if(this.props.roast && this.props.comments) {
       return (
         <div className="roast">
-            { this.renderHeadline() }
-            { this.renderRoastImage() }
-            <div className="roast-section">
-                <div>
-                  { this.renderSocials() }
-                  { this.renderScore() }
-                </div>
+          { this.renderHeadline() }
+          { this.renderRoastImage() }
+          <div className="roast-section">
+            <div>
+              { this.renderSocials() }
+              { this.renderScore() }
             </div>
-            { this.renderCommentSection(this.props.comments) }
+          </div>
+          { this.renderCommentSection(this.props.comments) }
         </div>
       );
     } else {
