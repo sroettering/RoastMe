b88d1a0b779500baca397e3a5802b6bd9d71beaf
Sven Röttering <svenroettering@googlemail.com>
2016-12-04 12:00:45 +0100

    show preview of image in upload dialog.

--- imports/modules/roasts/upload.jsx
+++ imports/modules/roasts/upload.jsx
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 
 export class ModalUpload extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filePath: "",
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ filePath: "" });
+  }
+
+  changeListener(event) {
+    const reader = new FileReader();
+    reader.readAsDataURL(this.fileInput.files[0]);
+    reader.onloadend = (event) => {
+      this.setState({ filePath: event.target.result });
+    }
+  }
+
   render() {
     return (
       <div className="modal">
@@ -9,7 +28,14 @@ export class ModalUpload extends Component {
         <label htmlFor="modal-input">Choose or drag image here</label>
         <div className="modal-upload">
           <span className="mdi mdi-upload"></span>
-          <input id="modal-input" type="file" name="name" value="" />
+          <input
+            id="modal-input"
+            type="file"
+            name="name"
+            value=""
+            ref={ (input) => this.fileInput = input }
+            onChange={ this.changeListener.bind(this) }/>
+          <img src={ this.state.filePath } />
         </div>
         <a href="#" className="button">Upload</a>
       </div>
