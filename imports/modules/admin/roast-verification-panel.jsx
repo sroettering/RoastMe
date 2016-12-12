f108dd1b2b2bcd37be220093c034a208555a0308
Sven Röttering <svenroettering@googlemail.com>
2016-12-12 20:10:58 +0100

    refactored admin dashboard

---
+++ imports/modules/admin/roast-verification-panel.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
+
+export class RoastVerificationPanel extends Component {
+
+  acceptRoast() {
+    const { roast } = this.props;
+    if(!roast) return;
+    Meteor.call('acceptRoast', roast._id);
+  }
+
+  declineRoast() {
+    const { roast } = this.props;
+    if(!roast) return;
+    Meteor.call('declineRoast', roast._id);
+  }
+
+  render() {
+    const { roast, numQueued } = this.props;
+    if(!roast) {
+      return (
+        <div className="wrapper">
+          <span>No Roasts today, Sir!</span>
+        </div>
+      );
+    } else {
+      return (
+        <div className="wrapper">
+          <div className="dashboard-panel">
+            <h3>Verification</h3>
+            <span>In Queue: { numQueued }</span>
+            <span>Title: { roast.title }</span>
+            <span>From: { roast.userName }</span>
+            <img className="roast-image" src={ roast.imageUrl } alt=""/>
+            <button className="button mdi mdi-thumb-up" onClick={ this.acceptRoast.bind(this) }>Accept</button>
+            <button className="button mdi mdi-thumb-down" onClick={ this.declineRoast.bind(this) }>Decline</button>
+          </div>
+        </div>
+      );
+    }
+  }
+}
+
+RoastVerificationPanel.propTypes = {
+  numQueued: React.PropTypes.number,
+};
