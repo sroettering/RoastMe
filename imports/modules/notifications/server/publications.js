a06ac361e2d67b9ac6e3f5fd54d86d740b6efc59
Sven Röttering <svenroettering@googlemail.com>
2017-07-17 18:02:03 +0200

    fix notification publication. it blocked the spiderable package from working

--- imports/modules/notifications/server/publications.js
+++ imports/modules/notifications/server/publications.js
@@ -3,14 +3,18 @@ import { Meteor } from 'meteor/meteor';
 import { Notifications } from '../notifications-collection';
 
 Meteor.publish('notification.unread', function() {
-  if(!this.userId) return;
-  return Notifications.find(
-    {
-      userId: this.userId,
-      status: 'unread'
-    }, {
-      sort: { createdAt: 1 },
-      fields: { updatedAt: 0 },
-      limit: 1,
-    });
+  if(this.userId) {
+    return Notifications.find(
+      {
+        userId: this.userId,
+        status: 'unread'
+      }, {
+        sort: { createdAt: 1 },
+        fields: { updatedAt: 0 },
+        limit: 1,
+      }
+    );
+  } else {
+    this.ready();
+  }
 });
