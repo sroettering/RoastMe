b2beb472288442e638547fd6f58c5ebdb52f9a0a
Sven Röttering <svenroettering@googlemail.com>
2017-07-28 22:21:05 +0200

    refactor infinite scrolling

--- imports/decorators/withRoasts.js
+++ imports/decorators/withRoasts.js
@@ -1,10 +1,19 @@
 import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker'
+import { ReactiveVar } from 'meteor/reactive-var';
 import { withMethodData } from 'meteor/orionsoft:react-meteor-data';
+
+const limit = new ReactiveVar(10);
 
 export default withMethodData((props, ready) => {
     const { category } = props;
     if(!category || category !== 'hot' && category !== 'trending' && category !== 'new') {
-        throw new Meteor.Error('props', 'allowed values are hot, new or trending');
+        throw new Meteor.Error('invalid props', 'allowed values are hot, new or trending');
     }
-    Meteor.call('getRoastsByCategory', category, 10, ready);
+    Tracker.autorun(() => {
+      Meteor.call('getRoastsByCategory', category, limit.get(), (error, response) => {
+        response.limit = limit;
+        ready(error, response);
+      });
+    });
 });
